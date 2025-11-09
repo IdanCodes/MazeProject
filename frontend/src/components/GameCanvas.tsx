4fc98885894d3e8a6a8a8bfc1f9283e9b9e38628
Idan Sahar <36777029+IdanCodes@users.noreply.github.com>
2025-11-09 19:28:42 +0200

    Smooth motion + player cell detection

--- frontend/src/components/GameCanvas.tsx
+++ frontend/src/components/GameCanvas.tsx
@@ -1,38 +1,93 @@
-import React, { useEffect, useRef } from "react";
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react";
 import { Maze } from "@shared/types/Maze";
 import { GridPos } from "@shared/types/GridPos";
 import { CellType } from "@shared/types/Grid";
 
-interface CanvasPos {
+export interface Vector2 {
   x: number;
   y: number;
 }
 
-const cellColor = (type: CellType): string =>
-  type === CellType.Passage ? "white" : "black";
+export function calcMagnitude(vec: Vector2): number {
+  return Math.sqrt(vec.x ** 2 + vec.y ** 2);
+}
 
-function GameCanvas({
-  maze,
-  cellScale,
-  playerPos,
-  playerColor = "purple",
-}: {
-  maze: Maze;
-  cellScale: number;
-  playerPos: GridPos;
-  playerColor?: string;
-}) {
+export const ZERO_VEC: Vector2 = { x: 0, y: 0 };
+
+export function calcNormalized(vec: Vector2): Vector2 {
+  const mag = calcMagnitude(vec);
+  return mag == 0
+    ? ZERO_VEC
+    : {
+        x: vec.x / mag,
+        y: vec.y / mag,
+      };
+}
+
+export function scaleVec(vec: Vector2, scale: number): Vector2 {
+  return {
+    x: vec.x * scale,
+    y: vec.y * scale,
+  };
+}
+
+export interface GameCanvasHandle {
+  gridToCanvas: (gridPos: GridPos) => Vector2;
+  canvasToGrid: (canvasPos: Vector2) => GridPos;
+  isMovableCell: (canvasPos: Vector2) => boolean;
+}
+
+const GameCanvas = forwardRef<
+  GameCanvasHandle,
+  {
+    maze: Maze;
+    cellScale: number;
+    playerPos: Vector2;
+    playerColor?: string;
+  }
+>(({ maze, cellScale, playerPos, playerColor = "purple" }, ref) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   const canvasWidth: number = cellScale * (maze.width + 1);
   const canvasHeight: number = cellScale * (maze.height + 1);
 
-  function getCanvasPos(gridPos: GridPos): CanvasPos {
+  function gridToCanvas(gridPos: GridPos): Vector2 {
     return {
       x: gridPos.col * cellScale,
       y: gridPos.row * cellScale,
     };
   }
+
+  function canvasToGrid(canvasPos: Vector2): GridPos {
+    return {
+      row: Math.floor(canvasPos.y / cellScale),
+      col: Math.floor(canvasPos.x / cellScale),
+    };
+  }
+
+  function canvasToVisualGrid(canvasPos: Vector2): GridPos {
+    return {
+      row: Math.round(Math.floor(canvasPos.y / cellScale) / 2) * 2,
+      col: Math.round(Math.floor(canvasPos.x / cellScale) / 2) * 2,
+    };
+  }
+
+  function isMovableCell(canvasPos: Vector2): boolean {
+    const gridPos = canvasToGrid(canvasPos);
+    console.log(gridPos);
+    return maze.inBounds(gridPos) && maze.getCell(gridPos) !== CellType.Wall;
+  }
+
+  useImperativeHandle(ref, () => ({
+    gridToCanvas,
+    canvasToGrid,
+    isMovableCell,
+  }));
 
   const fillCell = (
     ctx: CanvasRenderingContext2D,
@@ -42,7 +97,7 @@ function GameCanvas({
     if (style) ctx.fillStyle = style;
 
     // assume pos is in bounds
-    const canvasPos = getCanvasPos(pos);
+    const canvasPos = gridToCanvas(pos);
     ctx.fillRect(canvasPos.x, canvasPos.y, cellScale * 2, cellScale * 2);
   };
 
@@ -84,14 +139,14 @@ function GameCanvas({
 
         // vertical wall (up-down)
         if (maze.getCell(rightBar) === CellType.Wall) {
-          const rightCanvasPos = getCanvasPos({ row: i, col: j + 2 });
+          const rightCanvasPos = gridToCanvas({ row: i, col: j + 2 });
           ctx.moveTo(rightCanvasPos.x, rightCanvasPos.y - 1);
           ctx.lineTo(rightCanvasPos.x, rightCanvasPos.y + cellScale * 2 + 1);
         }
 
         // horizontal wall (left-right)
         if (maze.inBounds(downBar) && maze.getCell(downBar) === CellType.Wall) {
-          const downCanvasPos = getCanvasPos({ row: i + 2, col: j });
+          const downCanvasPos = gridToCanvas({ row: i + 2, col: j });
           ctx.moveTo(downCanvasPos.x - 1, downCanvasPos.y);
           ctx.lineTo(downCanvasPos.x + cellScale * 2 + 1, downCanvasPos.y);
         }
@@ -111,9 +166,23 @@ function GameCanvas({
     ctx.fillStyle = "rgb(245,245,245)";
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
+    // -- highlight player square
+    const position = {
+      x: playerPos.x - cellScale / 2,
+      y: playerPos.y - cellScale / 2,
+    };
+    const playerCellPos = gridToCanvas(canvasToVisualGrid(position));
+    ctx.fillStyle = "rgba(242,251,2,0.6)";
+    ctx.fillRect(
+      playerCellPos.x,
+      playerCellPos.y,
+      cellScale * 2,
+      cellScale * 2,
+    );
+
     drawGrid(ctx);
 
-    const position = getCanvasPos(playerPos);
+    // const position = gridToCanvas(playerPos);
     ctx.strokeStyle = "black";
     ctx.fillStyle = playerColor;
     ctx.lineWidth = 1;
@@ -121,7 +190,7 @@ function GameCanvas({
     ctx.arc(
       position.x + cellScale,
       position.y + cellScale,
-      cellScale * 0.6,
+      cellScale * 0.5,
       0,
       2 * Math.PI,
     );
@@ -150,6 +219,6 @@ function GameCanvas({
       ></canvas>
     </div>
   );
-}
+});
 
 export default GameCanvas;
--- frontend/src/components/GameManager.tsx
+++ frontend/src/components/GameManager.tsx
@@ -2,7 +2,13 @@ import React, { useEffect, useRef, useState } from "react";
 import usePlayerInputHandler from "../hooks/usePlayerPositionHandler";
 import { getMazeRenderHeight, MazeSize } from "../types/maze-size";
 import { Maze } from "@shared/types/Maze";
-import GameCanvas from "./GameCanvas";
+import GameCanvas, {
+  Vector2,
+  GameCanvasHandle,
+  calcNormalized,
+  scaleVec,
+  ZERO_VEC,
+} from "./GameCanvas";
 import {
   addPos,
   equalPos,
@@ -14,11 +20,12 @@ import { CellType, createRectGrid } from "@shared/types/Grid";
 import { generateDFSRectGrid } from "@shared/utils/maze-generator";
 import { getRandomInt } from "@shared/utils/common-helpers";
 import useAnimationUpdate from "../hooks/useAnimationUpdate";
+import gameCanvas from "./GameCanvas";
 
 function GameManager({
   mazeSize,
   mazeScale,
-  fps = 5,
+  fps = 60,
   genFlag,
 }: {
   mazeSize: MazeSize;
@@ -27,78 +34,104 @@ function GameManager({
   genFlag: { flag: boolean; setFlag: (value: boolean) => void };
 }) {
   const [maze, setMaze] = useState<Maze>(new Maze(createRectGrid(5, 5)));
+  const gameCanvasRef = useRef<GameCanvasHandle | null>(null);
   const [playerPos, setPlayerPos] = useState<GridPos>({ row: 0, col: 0 });
   const inputVector = useRef<GridPos>({ row: 0, col: 0 });
+  const [canvasPos, setCanvasPos] = useState<Vector2>({ x: 0, y: 0 });
+  const velocity = useRef<Vector2>({ x: 0, y: 0 });
+  const speedAmplifier = 5;
+
   const cellScale = getMazeRenderHeight(mazeSize) / maze.height;
-
-  function getNextPos(pos: GridPos, inputVec: GridPos): GridPos {
-    let delta: GridPos = {
-      row: inputVec.row > 0 ? 2 : inputVec.row < 0 ? -2 : 0,
-      col: inputVec.col > 0 ? 2 : inputVec.col < 0 ? -2 : 0,
-    };
-
-    const targetPos = addPos(pos, delta);
-    const targetBridge = addPos(pos, inputVec);
-    if (!isDiagonalVector(inputVec)) {
-      if (
-        !maze.inBounds(targetPos) ||
-        maze.getCell(targetBridge) === CellType.Wall
-      )
-        return pos;
-      return targetPos;
-    }
-
-    const verticalBridge: GridPos = {
-      row: pos.row + inputVec.row,
-      col: pos.col,
-    };
-    const verticalTarget: GridPos = {
-      row: pos.row + delta.row,
-      col: pos.col,
-    };
-    if (
-      !maze.inBounds(verticalTarget) ||
-      maze.getCell(verticalBridge) === CellType.Wall
-    )
-      delta.row = 0;
-
-    const horizontalBridge: GridPos = {
-      row: pos.row,
-      col: pos.col + inputVec.col,
-    };
-    const horizontalTarget: GridPos = {
-      row: pos.row,
-      col: pos.col + delta.col,
-    };
-    if (
-      !maze.inBounds(horizontalTarget) ||
-      maze.getCell(horizontalBridge) === CellType.Wall
-    )
-      delta.col = 0;
-
-    // diagonal -> horizontal
-    if (isDiagonalVector(delta)) delta.row = 0;
-
-    return addPos(pos, delta);
-  }
-
-  const loopNumber = useRef<number>(0);
-  usePlayerInputHandler((deltaPos: GridPos) => {
-    inputVector.current = deltaPos;
-    loopNumber.current = (loopNumber.current + 1) % 5;
-    moveLoop(loopNumber.current);
+  usePlayerInputHandler((inputVec) => {
+    velocity.current = scaleVec(
+      calcNormalized({
+        x: inputVec.col,
+        y: inputVec.row,
+      }),
+      speedAmplifier,
+    );
   });
 
-  function moveLoop(loopN: number) {
-    if (loopNumber.current != loopN || equalPos(inputVector.current, ZERO_POS))
-      return;
+  useAnimationUpdate(fps, () => {
+    setCanvasPos((cp) => {
+      const newPos: Vector2 = {
+        x: cp.x + velocity.current.x,
+        y: cp.y + velocity.current.y,
+      };
+      if (gameCanvasRef.current && gameCanvasRef.current.isMovableCell(newPos))
+        return newPos;
+      return cp;
+    });
+  });
 
-    setPlayerPos((pos) => getNextPos(pos, inputVector.current));
-
-    setTimeout(() => {
-      moveLoop(loopN);
-    }, 1000 / fps);
-  }
+  // function getNextPos(pos: GridPos, inputVec: GridPos): GridPos {
+  //   let delta: GridPos = {
+  //     row: inputVec.row > 0 ? 2 : inputVec.row < 0 ? -2 : 0,
+  //     col: inputVec.col > 0 ? 2 : inputVec.col < 0 ? -2 : 0,
+  //   };
+  //
+  //   const targetPos = addPos(pos, delta);
+  //   const targetBridge = addPos(pos, inputVec);
+  //   if (!isDiagonalVector(inputVec)) {
+  //     if (
+  //       !maze.inBounds(targetPos) ||
+  //       maze.getCell(targetBridge) === CellType.Wall
+  //     )
+  //       return pos;
+  //     return targetPos;
+  //   }
+  //
+  //   const verticalBridge: GridPos = {
+  //     row: pos.row + inputVec.row,
+  //     col: pos.col,
+  //   };
+  //   const verticalTarget: GridPos = {
+  //     row: pos.row + delta.row,
+  //     col: pos.col,
+  //   };
+  //   if (
+  //     !maze.inBounds(verticalTarget) ||
+  //     maze.getCell(verticalBridge) === CellType.Wall
+  //   )
+  //     delta.row = 0;
+  //
+  //   const horizontalBridge: GridPos = {
+  //     row: pos.row,
+  //     col: pos.col + inputVec.col,
+  //   };
+  //   const horizontalTarget: GridPos = {
+  //     row: pos.row,
+  //     col: pos.col + delta.col,
+  //   };
+  //   if (
+  //     !maze.inBounds(horizontalTarget) ||
+  //     maze.getCell(horizontalBridge) === CellType.Wall
+  //   )
+  //     delta.col = 0;
+  //
+  //   // diagonal -> horizontal
+  //   if (isDiagonalVector(delta)) delta.row = 0;
+  //
+  //   return addPos(pos, delta);
+  // }
+  //
+  // const loopNumber = useRef<number>(0);
+  // usePlayerInputHandler((deltaPos: GridPos) => {
+  //   inputVector.current = deltaPos;
+  //   loopNumber.current = (loopNumber.current + 1) % 5;
+  //   moveLoop(loopNumber.current);
+  // });
+  //
+  // function moveLoop(loopN: number) {
+  //   if (loopNumber.current != loopN || equalPos(inputVector.current, ZERO_POS))
+  //     return;
+  //
+  //   setPlayerPos((pos) => getNextPos(pos, inputVector.current));
+  //
+  //   setTimeout(() => {
+  //     moveLoop(loopN);
+  //   }, 1000 / fps);
+  // }
 
   function generateMaze() {
     const grid = generateDFSRectGrid(mazeScale * 2 - 1, mazeScale * 2 - 1);
@@ -117,12 +150,18 @@ function GameManager({
   useEffect(() => {
     if (!genFlag.flag) return;
 
-    console.log("generate");
     genFlag.setFlag(false);
     generateMaze();
   }, [mazeSize, genFlag.flag]);
 
-  return <GameCanvas maze={maze} cellScale={cellScale} playerPos={playerPos} />;
+  return (
+    <GameCanvas
+      ref={gameCanvasRef}
+      maze={maze}
+      cellScale={cellScale}
+      playerPos={canvasPos}
+    />
+  );
 }
 
 export default GameManager;
