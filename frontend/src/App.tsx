289a4019336b3c3f4f7b62cc39836ac70427900f
Idan Sahar <36777029+IdanCodes@users.noreply.github.com>
2025-11-10 18:57:36 +0200

    Network server communication demo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,88 +1,10 @@
 import "./index.css";
-import { ChangeEvent, useRef, useState } from "react";
-import { MazeSize } from "./types/maze-size";
-import { Maze } from "@shared/types/Maze";
-import { createRectGrid } from "@shared/types/Grid";
-import GameManager from "./components/GameManager";
+import NetworkTests from "./pages/NetworkTests";
+import MazeTests from "./pages/MazeTests";
 
 function App() {
-  const [mazeSize, setMazeSize] = useState({
-    size: 5,
-    scale: "Small",
-    mazeSize: MazeSize.Small,
-  });
-  const [currMaze, setCurrMaze] = useState(new Maze(createRectGrid(5, 5)));
-  const [genFlag, setGenFlag] = useState<boolean>(true);
-
-  function handleSizeChange(e: ChangeEvent<HTMLInputElement>) {
-    e.preventDefault();
-
-    if (Number(e.target.value) <= 0) return;
-
-    setMazeSize((s) => ({
-      ...s,
-      [e.target.name]: e.target.value,
-    }));
-  }
-
-  function strToSize(s: string): MazeSize {
-    switch (s) {
-      case "XS":
-        return MazeSize.XS;
-      case "Small":
-        return MazeSize.Small;
-      case "Medium":
-        return MazeSize.Medium;
-      case "Large":
-        return MazeSize.Large;
-      case "XL":
-        return MazeSize.XL;
-      default:
-        return MazeSize.Medium;
-    }
-  }
-
-  function handleUpdateMazeSize(e: ChangeEvent<HTMLSelectElement>) {
-    setMazeSize((s) => ({ ...s, mazeSize: strToSize(e.target.value) }));
-  }
-
-  return (
-    <>
-      <div className="flex flex-row">
-        <p className="text-3xl">Size:</p>
-        <input
-          type="number"
-          name="size"
-          className="border-2 text-3xl"
-          value={mazeSize.size}
-          onChange={handleSizeChange}
-        />
-        <select
-          id="mazeSizes"
-          name="mazeSize"
-          size={6}
-          onChange={handleUpdateMazeSize}
-        >
-          <option value="XS">XS</option>
-          <option value="Small">Small</option>
-          <option value="Medium">Medium</option>
-          <option value="Large">Large</option>
-          <option value="XL">XL</option>
-        </select>
-      </div>
-      <br />
-      <button onClick={() => setGenFlag(true)} className="bg-gray-500 text-3xl">
-        Generate maze
-      </button>
-
-      {/*<MazeRenderer maze={currMaze} size={mazeSize.mazeSize} />*/}
-      <GameManager
-        mazeSize={mazeSize.mazeSize}
-        mazeScale={mazeSize.size}
-        genFlag={{ flag: genFlag, setFlag: setGenFlag }}
-      />
-    </>
-  );
+  return <NetworkTests />;
+  // return <MazeTests />;
 }
 
 export default App;
---
+++ frontend/src/pages/NetworkTests.tsx
@@ -0,0 +1,110 @@
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+import clsx from "clsx";
+
+const SERVER_PORT = 3003;
+const SERVER_IP = "127.0.0.1";
+const SERVER_WS_URL: string = `ws://${SERVER_IP}:${SERVER_PORT}`;
+
+interface Message {
+  time: number;
+  text: string;
+  type: string;
+}
+
+function NetworkTests() {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const wsRef = useRef<WebSocket>(null);
+  const isConnected = wsRef.current?.readyState === WebSocket.OPEN;
+
+  useEffect(() => {
+    if (wsRef.current) return;
+    addMessage(`Attempting to connect to ${SERVER_WS_URL}...`, "system");
+
+    wsRef.current = new WebSocket(SERVER_WS_URL);
+
+    wsRef.current.onopen = () => {
+      addMessage("WebSocket Connected to Node.js intermediary", "system");
+    };
+
+    wsRef.current.onmessage = onReceiveMessage;
+
+    wsRef.current.onerror = (error) => {
+      addMessage(
+        `WebSocket error has occurred "${JSON.stringify(error)}"`,
+        "system",
+      );
+    };
+
+    return () => {
+      // Close the WebSocket connection when the component unmounts
+      console.log("closing", isConnected);
+      if (isConnected) wsRef.current!.close();
+    };
+  }, []);
+
+  const addMessage = (text: string, type: string) => {
+    setMessages((prev) => [...prev, { time: Date.now(), text, type }]);
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    e.preventDefault();
+    setInputValue(e.target.value);
+  };
+
+  function sendMessage() {
+    const toSend = inputValue.trim();
+    if (toSend.length == 0 || !wsRef.current) return;
+    if (!isConnected)
+      return addMessage("Cannot send - WebSocket is not connected", "system");
+
+    const socket = wsRef.current;
+    socket.send(toSend);
+    addMessage(toSend, "sent");
+    setInputValue(""); // clear
+  }
+
+  function onReceiveMessage(event: MessageEvent) {
+    addMessage(event.data, "received");
+  }
+
+  return (
+    <div className="w-80 mx-auto flex flex-col justify-center h-fit border-5 gap-10 p-5">
+      <input
+        className="border-2 rounded-xl text-xl p-2"
+        value={inputValue}
+        placeholder="text to send..."
+        type="text"
+        onChange={handleInputChange}
+      />
+      <button
+        className={clsx("border-2 text-xl font-bold cursor-pointer")}
+        disabled={inputValue.length == 0}
+        onClick={sendMessage}
+      >
+        Send Message
+      </button>
+
+      <h2 className="text-xl font-semibold mb-3 text-gray-700">
+        Communication Log
+      </h2>
+      <div className="message-box p-4 space-y-2">
+        {messages
+          .slice()
+          .reverse()
+          .map((msg) => (
+            <p key={msg.time} className={`text-sm ${msg.type}`}>
+              {new Date(msg.time).toLocaleTimeString()}[{msg.type.toUpperCase()}
+              ] {msg.text}
+            </p>
+          ))}
+      </div>
+      <div className="text-xs text-gray-400 mt-4">
+        Target: `{SERVER_WS_URL}` (Python UDP Server)
+      </div>
+    </div>
+  );
+}
+
+export default NetworkTests;
