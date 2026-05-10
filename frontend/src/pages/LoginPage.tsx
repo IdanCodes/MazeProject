9f065661456164084d88b4982aa0aff2780b0810
Idan Sahar <36777029+IdanCodes@users.noreply.github.com>
2026-05-10 12:39:35 +0300

    Login page submit on enter

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -54,40 +54,40 @@ export function LoginPage({
           Back
         </PrimaryButton>
         <h1 className="text-3xl text-center">Log In</h1>
-        <form
-          onSubmit={doLogin}
-          className="my-4 mx-auto w-4/5 py-5 flex gap-3 flex-col border-3 rounded-2xl"
-        >
-          <div className="flex justify-around">
-            <p className="text-3xl">Username:</p>
-            <input
-              className="text-2xl border-2 rounded-xl py-1 px-1"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            ></input>
+        <form onSubmit={doLogin}>
+          <div className="my-4 mx-auto w-4/5 py-5 flex gap-3 flex-col border-3 rounded-2xl">
+            <div className="flex justify-around">
+              <p className="text-3xl">Username:</p>
+              <input
+                className="text-2xl border-2 rounded-xl py-1 px-1"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              ></input>
+            </div>
+            <div className="flex justify-around">
+              <p className="text-3xl">Password:</p>
+              <input
+                className="text-2xl border-2 rounded-xl py-1 px-1 "
+                placeholder="Password"
+                value={password}
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
           </div>
-          <div className="flex justify-around">
-            <p className="text-3xl">Password:</p>
-            <input
-              className="text-2xl border-2 rounded-xl py-1 px-1 "
-              placeholder="Password"
-              value={password}
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
+          <div className="mx-auto w-fit my-2">
+            <ErrorLabel text={loginError} />
           </div>
+          <PrimaryButton
+            btnType="submit"
+            className="mx-auto text-2xl"
+            onClick={doLogin}
+            disabled={disableLogin}
+          >
+            Log In
+          </PrimaryButton>
         </form>
-        <div className="mx-auto w-fit my-2">
-          <ErrorLabel text={loginError} />
-        </div>
-        <PrimaryButton
-          className="mx-auto text-2xl"
-          onClick={doLogin}
-          disabled={disableLogin}
-        >
-          Log In
-        </PrimaryButton>
       </div>
     </>
   );
