032648a09044c452485863409ac89c603c8b14b9
Idan Sahar <36777029+IdanCodes@users.noreply.github.com>
2026-05-20 10:47:29 +0300

    Add electron forge

--- frontend/src/components/buttons/PrimaryButton.tsx
+++ frontend/src/components/buttons/PrimaryButton.tsx
@@ -31,7 +31,7 @@ export default function PrimaryButton({
       <motion.button
         className={clsx(
           `box-border flex scale-95 items-center justify-center rounded-xl p-3 transition-all duration-100 select-none`,
-          `bg-[rgb(106,114,130)] hover:bg-[rgba(106,114,130,0.9)] active:bg-[rgba(74,85,101, 0.9)]`,
+          `bg-[rgb(106,114,130)] hover:bg-[rgba(106,114,130,0.9)] active:bg-[rgba(74,85,101, 0.9)] `,
           disabled && "cursor-not-allowed opacity-70",
           !disabled && `cursor-pointer hover:scale-100`,
           className,
